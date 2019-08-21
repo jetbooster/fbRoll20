71133f54ce5eae3f35407876b176b10249874054
jetbooster <jarvisam@gmail.com>
2019-08-21 20:32:21 +0100

    regex is hrd

--- roller.py
+++ roller.py
@@ -2,8 +2,8 @@ import re
 from random import randint
 
 def parse(text):
-  trimSummon = text.replace('!!roll','').replace('\n','').strip()
-  matchResult = re.match(r".*(?P<dice>\d{0,2}d\d{0,3}\s?([+-]\s?\d{0,2})?).*", trimSummon)
+  trimSummon = text.replace('\n','').split('!!roll')[1].strip()
+  matchResult = re.match(r"(?P<dice>\d{0,2}d\d{0,3}\s?([+-]\s?\d{0,2})?).*", trimSummon)
   parsedSection = matchResult.group('dice')
   [numDie, rest] = [elem.strip() for elem in parsedSection.split('d')]
   if numDie == '':
@@ -37,3 +37,6 @@ def buildResponse(text):
   result = roll(parsedRoll)
   responseContent += f"Rolled: {result['rolledDie']}\nTotal (including modifier):{result['total']}"
   return responseContent
+
+x = buildResponse("blah blah\n !!roll 4d20")
+print(x)
